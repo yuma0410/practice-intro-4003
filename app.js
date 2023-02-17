c3c89792471db0adbed651113877b5a19019b5be
yuma0410 <115466706+yuma0410@users.noreply.github.com>
2023-02-17 14:57:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -53,12 +53,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
@@ -83,6 +78,12 @@ app.get('/logout', function (req, res, next) {
   });
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
